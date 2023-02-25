f99ce5176da32898a4bdb3e62b5cde355fd61ee6
surajbhan-3 <surajbhan2boaz@gmail.com>
2023-02-26 00:15:17 +0530

    day-5

--- frontend/index.js
+++ frontend/index.js
@@ -2,8 +2,18 @@
 console.log('hello')
 
 
-let btn=document.getElementById("furniture")
-btn.addEventListener("click", ()=>{
+// let btn=document.getElementById("furniture")
+// btn.addEventListener("click", ()=>{
 
-     console.log('hellodsfddsfds')
+//      console.log('hellodsfddsfds')
+// })
+
+ fetch("https://dull-belt-colt.cyclic.app/items/furniture")
+// fetch("http://localhost:4500/items/furniture")
+.then((res)=>{
+     return res.json()
+}).then((data)=>{
+     console.log(data)
+}).catch((err)=>{
+     console.log(err)
 })
---
+++ frontend/signup.js
@@ -0,0 +1,34 @@
+
+ console.log("hello")
+let form=document.querySelector("form")
+
+form.addEventListener("submit",(e)=>{
+     e.preventDefault()
+      console.log("hello")
+      let payload={
+
+        name:form.name.value,
+        email:form.email.value,
+        password:form.password.value,
+       
+      }
+ 
+
+      fetch("https://dull-belt-colt.cyclic.app/users/register",{
+        method:"POST",
+        headers:{
+            "Content-Type":"application/json"
+        },
+        body:JSON.stringify(payload)
+    
+      }).then((res)=>{
+        return  res.json()
+
+      }).then((res)=>{
+        console.log(res)
+      }).catch((err)=>{
+        console.log(err)
+      })
+   
+      
+})
